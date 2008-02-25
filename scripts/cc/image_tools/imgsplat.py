2bdc518133f1da222339435da03a95c378881f84
nyergler <nyergler@committer.creativecommons.org>
2008-02-25 18:07:50 +0000

    Calculate paths with one less assumption -- still assuming that we're operating in a checkout-like layout, but no assuming what path we're operating from.

--- scripts/cc/image_tools/imgsplat.py
+++ scripts/cc/image_tools/imgsplat.py
@@ -17,6 +17,16 @@ from rdflib import Namespace
 NS_CC = Namespace("http://creativecommons.org/ns#")
 NS_RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 
+def checkout_base():
+    """Return the base destination path.  Note that this assumes you're
+    running the tool from a Subversion checkout (or that your filesystem
+    layout is the same as our repository)."""
+
+
+    return os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', '..', '..')
+        )
+
 def load_graph(filename):
     """Load the specified filename; return a graph."""
 
@@ -33,10 +43,13 @@ def load_graph(filename):
 def copyto(source, dest, code, size, jurisdiction, money):
     if (not os.access(dest, os.F_OK)):
 	os.makedirs(dest)
-    dest += size+'.png'
+
+    dest = os.path.join(dest, size + '.png')
+
     if string.find(code, 'nc') != -1 and money.has_key(jurisdiction):
 	source += '_'+money[jurisdiction]
     source += '.png'
+
     try:
 	shutil.copy2(source, dest)
     except:
@@ -65,7 +78,9 @@ def splat(license_graph):
 	code = m.group(1)
 	version = m.group(3)
 	jurisdiction = m.group(5)
-	dest = '../www/l/'+code+'/'
+
+	dest = os.path.join(checkout_base(), 'www', 'l', code)
+
 	code2 = code
 	size = '88x31'
 	if (code == 'by-nd-nc'):
@@ -75,12 +90,13 @@ def splat(license_graph):
 	elif (code == 'LGPL' or code == 'GPL'):
 	    size = '88x62'
         if (version):
-	    dest += version+'/'
+	    dest = os.path.join(dest, version)
         if (jurisdiction):
-	    dest += jurisdiction+'/'
-	source = '../base-images/'+size+'/'+code2
+	    dest = os.path.join(dest, jurisdiction)
+
+        source = os.path.join(checkout_base(), 'base-images', size, code2)
 	copyto(source, dest, code, size, jurisdiction, money)
-	source = '../base-images/'+'80x15'+'/'+code2
+        source = os.path.join(checkout_base(), 'base-images', '80x15', code2)
 	copyto(source, dest, code, '80x15', '', money)
 
 def cli():
