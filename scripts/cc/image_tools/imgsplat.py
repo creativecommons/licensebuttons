41dfa6b656418380ae6051ba375bc153e13e6896
cwebber <cwebber@committer.creativecommons.org>
2011-01-04 18:07:04 +0000

    Updated imgsplat to look at FOAF:logo assertions in RDF

--- scripts/cc/image_tools/imgsplat.py
+++ scripts/cc/image_tools/imgsplat.py
@@ -16,6 +16,22 @@ from rdflib import Namespace
 
 NS_CC = Namespace("http://creativecommons.org/ns#")
 NS_RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+NS_FOAF = Namespace("http://xmlns.com/foaf/0.1/")
+
+MONEY = {
+    'at': 'euro',
+    'be': 'euro',
+    'fi': 'euro',
+    'de': 'euro',
+    'es': 'euro',
+    'fr': 'euro',
+    'gr': 'euro',
+    'ie': 'euro',
+    'it': 'euro',
+    'lu': 'euro',
+    'nl': 'euro',
+    'pt': 'euro'}
+
 
 def checkout_base():
     """Return the base destination path.  Note that this assumes you're
@@ -40,14 +56,18 @@ def load_graph(filename):
 
     return store
 
-def copyto(source, dest, code, size, jurisdiction, money):
-    if (not os.access(dest, os.F_OK)):
-	os.makedirs(dest)
+def copyto(source, dest, code, size, jurisdiction, money=MONEY):
+    # Make the destination directory, if necessary
+    dest_dir = os.path.dirname(dest)
+    if (not os.access(dest_dir, os.F_OK)):
+	os.makedirs(dest_dir)
 
-    dest = os.path.join(dest, size + '.png')
+    # If NC (and not the 80x15 icon), use the appropriate currency icon.
+    if ((string.find(code, 'nc') != -1
+         and money.has_key(jurisdiction)
+         and size != '80x15')):
+	source += '_' + money[jurisdiction]
 
-    if string.find(code, 'nc') != -1 and money.has_key(jurisdiction):
-	source += '_'+money[jurisdiction]
     source += '.png'
 
     try:
@@ -55,49 +75,40 @@ def copyto(source, dest, code, size, jurisdiction, money):
     except:
 	print 'Failed to copy '+source+' to '+dest
 
+
 def splat(license_graph):
-
-    money = {
-        'at': 'euro',
-        'be': 'euro',
-        'fi': 'euro',
-        'de': 'euro',
-	'es': 'euro',
-	'fr': 'euro',
-        'gr': 'euro',
-        'ie': 'euro',
-        'it': 'euro',
-        'lu': 'euro',
-        'nl': 'euro',
-        'pt': 'euro',
-    }
-
     for uri in license_graph.subjects(NS_RDF.type, NS_CC.License):
         print uri
-	m = re.search('http://creativecommons.org/(?P<group>licenses|publicdomain)/(?P<code>.*?)/((?P<version>.*?)/((?P<jurisdiction>.*?)/)?)?', str(uri))
-	code = m.group('code')
-	version = m.group('version')
-	jurisdiction = m.group('jurisdiction')
 
-	dest = os.path.join(checkout_base(), 'www', m.group('group')[0], code)
+        for s, p, logo in license_graph.triples((uri, NS_FOAF.logo, None)):
+            # Get the dest_path, which is base-images and
+            # everything after 'http://i.creativecommons.org/'
+            dest_path = os.path.join(
+                checkout_base(), 'www', str(logo)[29:])
 
-	code2 = code
-	size = '88x31'
-	if (code == 'by-nd-nc'):
-	    code2 = 'by-nc-nd'
-	elif (code == 'nc' or code == 'nd' or code == 'sa' or code == 'nd-nc' or code == 'nc-sa'):
-	    code2 = 'somerights1'
-	elif (code == 'LGPL' or code == 'GPL'):
-	    size = '88x62'
-        if (version):
-	    dest = os.path.join(dest, version)
-        if (jurisdiction):
-	    dest = os.path.join(dest, jurisdiction)
+            m = re.search(
+                '^http://i.creativecommons.org/'
+                '(?P<group>l|p)/'
+                '(?P<code>.*?)/'
+                '((?P<version>.*?)/'
+                '((?P<jurisdiction>.*?)/)?)?'
+                '(?P<size>.*)\.png$', str(logo))
 
-        source = os.path.join(checkout_base(), 'base-images', size, code2)
-	copyto(source, dest, code, size, jurisdiction, money)
-        source = os.path.join(checkout_base(), 'base-images', '80x15', code2)
-	copyto(source, dest, code, '80x15', '', money)
+            code = m.group('code')
+            jurisdiction = m.group('jurisdiction')
+            size = m.group('size')
+
+            code2 = code
+            if (code == 'by-nd-nc'):
+                code2 = 'by-nc-nd'
+            elif code in ('nc', 'nd', 'sa', 'nd-nc', 'nc-sa'):
+                code2 = 'somerights1'
+
+            source = os.path.join(
+                checkout_base(), 'base-images', size, code2)
+
+            copyto(source, dest_path, code, size, jurisdiction)
+
 
 def cli():
     """imgsplat command line interface."""
